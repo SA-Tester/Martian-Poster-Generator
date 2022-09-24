64835171ae50bc1bde52d255c481c50190d1d2c1
@SIA <59891156+SA-Tester@users.noreply.github.com>
2022-09-24 12:09:52 +0530

    trained model for image colorization

--- desktop-app/main.py
+++ desktop-app/main.py
@@ -79,3 +79,25 @@ def createWindow():
     return window.mainloop()
 
 createWindow()
+
+# import tensorflow as tf
+# load the model
+# load_model = tf.keras.models.load_model('Saved Models')
+
+# color_me = []
+# for filename in os.listdir('images/black and white/256X256'):
+#         color_me.append(tf.keras.utils.img_to_array(tf.keras.utils.load_img('images/black and white/256X256/'+filename)))
+# color_me = np.array(color_me, dtype=float)
+# color_me = rgb2lab(1.0/255*color_me)[:,:,:,0]
+# color_me = color_me.reshape(color_me.shape+(1,))
+
+# Predict Images
+# output = load_model.predict(color_me)
+# output = output * 128
+
+# # Output colorizations
+# for i in range(len(output)):
+#     cur = np.zeros((256, 256, 3))
+#     cur[:,:,0] = color_me[i][:,:,0]
+#     cur[:,:,1:] = output[i]
+#     imsave("images/colored_results/img_"+str(i)+".jpg", lab2rgb(cur)*255)
